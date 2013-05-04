8cfaa4215f440ef2f3d879354b1f5d188d480693
Dylan Madisetti <dylan.madisetti@gmail.com>
2013-05-03 21:15:22 -0400

    init

---
+++ hello.js
@@ -0,0 +1,20 @@
+var express = require('express'),
+fs = require('fs'),
+mustache = require('mustache');
+
+var app = express.createServer();
+
+app.use("/goodies", express.static(__dirname + '/goodies'));
+
+app.get('/', function(request, response) {
+  fs.readFile( __dirname+'\\index.html', function (err, data) {
+    if (err) response.send('woops');
+    var code = data.toString();
+    response.send(mustache.render(code));
+  });
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
