ba7b8785992ccf9dd4523e2677353476153659f1
Dylan Madisetti <dylan.madisetti@gmail.com>
2013-10-11 06:32:37 -0400

    Just hacking it out

--- goodies/the.js
+++ goodies/the.js
@@ -2,14 +2,24 @@ var Slideshow = function(){
 	this.radios = document.getElementsByName('slideshow');
 	this.pictures = document.getElementsByName('slide');
 	this.display = 0;
-	this.clicker = function(e){
-		slideshow.pictures[slideshow.display].className="";
-		console.log(e);
-		slideshow.display = this.value;
-		slideshow.pictures[slideshow.display].className="active";
+	this.interval = 50;
+	this.time = this.interval;
+	this.change=function(value){
+		this.pictures[this.display].className="";
+		this.display = value;
+		this.pictures[value].className="active";		
+		this.radios[value].checked=true;
+		this.time = this.interval;
 	}
-	var i = this.radios.length;
+	this.clicker = function(e){
+		slideshow.change(parseInt(this.value));
+	}
+
+	var i= this.radios.length;
 	while(i--){this.radios[i].onclick=this.clicker;}
 }
 
 var slideshow = new Slideshow();
+window.setInterval(function(){
+	if(!slideshow.time--) slideshow.change(slideshow.display + 1 == slideshow.radios.length ? 0 : slideshow.display + 1);
+},100);
