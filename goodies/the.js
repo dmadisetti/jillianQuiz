77400f758320db8e7f9d2b05f6d1ed45b205fda1
Dylan Madisetti <dylan.madisetti@gmail.com>
2013-10-22 16:13:43 -0400

    added fade

--- goodies/the.js
+++ goodies/the.js
@@ -4,16 +4,35 @@ var Slideshow = function(){
 	this.radios = document.getElementsByName('slideshow');
 	this.pictures = document.getElementsByName('slide');
 	this.display = 0;
-	this.interval = 50;
+	this.interval = 150;
+	this.fadeInterval = 1000;
+	this.fadeChange=1/this.fadeInterval;
+	this.fade = this.fadeInterval;
 	this.time = this.interval;
 
 	// Helper functions
 	this.change=function(value){
-		this.pictures[this.display].className="";
-		this.display = value;
-		this.pictures[value].className="active";		
+		this.pictures[this.display].className="top";
+		this.pictures[this.display].style.opacity = 1;
+		this.next = value;
+		this.pictures[value].className="active";
+		this.pictures[value].style.opacity = 1;
 		this.radios[value].checked=true;
 		this.time = this.interval;
+		window.clearInterval(this.fader || 0);
+		this.fader = window.setInterval(function(){
+				slideshow.dissolve();
+		},this.fadeIteration);
+	}
+	this.dissolve= function(){
+		if(this.fade--){
+			this.pictures[this.display].style.opacity -= this.fadeChange;
+		}else{
+			this.pictures[this.display].className = "";
+			slideshow.display = slideshow.next;
+			this.fade = this.fadeInterval;
+			window.clearInterval(slideshow.fader || 0);
+		}
 	}
 	this.clicker = function(e){
 		slideshow.change(parseInt(this.value));
